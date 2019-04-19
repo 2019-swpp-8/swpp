c5b85854410b3d738562a1ee5d5c4e734cc2dc14
Do-iM <daha047@gmail.com>
2019-04-19 16:07:22 +0900

    t

--- backend/swpp/views.py
+++ backend/swpp/views.py
@@ -1,3 +1,4 @@
 from swpp.view.user import UserList, UserDetails
 from swpp.view.profile import ProfileList, ProfileDetails
 from swpp.view.tutor import TutorList, TutorDetails
+from swpp.view.tutor import TutorList, TutorDetails
