d4f94a4af5991d518419a1294c199c12f5167ea3
Do-iM <daha047@gmail.com>
2019-05-05 16:14:39 +0900

    Add Tutor Put Request

--- backend/swpp/view/tutor.py
+++ backend/swpp/view/tutor.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from swpp.models import Tutor
 from swpp.serializers import TutorSerializer
+from swpp.permissions import IsOwner
 from rest_framework import generics
 
 class TutorList(generics.ListAPIView):
     queryset = Tutor.objects.all()
     serializer_class = TutorSerializer
 
-class TutorDetails(generics.RetrieveAPIView):
+class TutorDetails(generics.RetrieveUpdateAPIView):
+    permission_classes = (IsOwner,)
     queryset = Tutor.objects.all()
     serializer_class = TutorSerializer
+
+    def put(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
