4bb4d8e495696877320f41dee6915d02bba91ce3
Dongmin Lee <ldm@ldm2468.com>
2019-05-22 19:47:06 +0900

    Fix CORS permissions bug

--- backend/swpp/permissions.py
+++ backend/swpp/permissions.py
@@ -4,4 +4,6 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
+        if request.user.is_anonymous:
+            return False
         return obj == request.user.profile or obj == request.user.profile.tutor
