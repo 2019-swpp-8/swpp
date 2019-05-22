3e1c0f1200569dcd4e60c73834d9df218f40b5cc
Dongmin Lee <ldm@ldm2468.com>
2019-05-22 20:04:58 +0900

    Add profile name to backend

--- backend/swpp/model/profile.py
+++ backend/swpp/model/profile.py
@@ -5,6 +5,7 @@ from swpp.model.tag import Tag
 
 class Profile(models.Model):
     user = AutoOneToOneField(User, primary_key=True, on_delete=models.CASCADE, null = False)
+    name = models.TextField(default = '')
     joined = models.DateTimeField(auto_now_add = True)
     major = models.TextField(default = '')
     contact = models.TextField(default = '010-0000-0000')
