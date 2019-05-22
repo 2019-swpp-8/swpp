a0ddedc29597275839d7cbbc9447fce8cc5fa19a
Dongmin Lee <ldm@ldm2468.com>
2019-05-22 21:13:02 +0900

    Change permission logic

--- backend/swpp/permissions.py
+++ backend/swpp/permissions.py
@@ -4,6 +4,6 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        if request.user.is_anonymous:
-            return False
-        return obj == request.user.profile or obj == request.user.profile.tutor
+        if request.user.is_authenticated:
+            return obj == request.user.profile or obj == request.user.profile.tutor
+        return False
