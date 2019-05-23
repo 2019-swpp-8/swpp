bceb65e0e353d1b8527f0f1239ffd69527420b10
Dongmin Lee <ldm@ldm2468.com>
2019-05-23 13:55:36 +0900

    회원가입 한글화 패치

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
-from registration_defaults.settings import *
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -36,7 +34,6 @@ STATIC_URL = '/static/'
 # Application definition
 
 INSTALLED_APPS = [
-    'registration_defaults',
     'django.contrib.admin',
     'registration',
     'django.contrib.auth',
