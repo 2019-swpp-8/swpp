f334f975f348ebd6299baa4870e313eb5f20842c
Dongmin Lee <ldm@ldm2468.com>
2019-05-29 23:14:03 +0900

    Make tutor list rows clickable

--- frontend/src/components/atoms/TutorRow/index.js
+++ frontend/src/components/atoms/TutorRow/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import _ from 'lodash'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 
 const TutorRow = ({tutor}) => {
   return typeof tutor === 'undefined' ? null :
-  <tr>
-    <td> {tutor['profile']['name']} </td>
+  <tr className="position-relative">
+    <td> <Link className="stretched-link" to={'/profile/' + tutor['profile']['user']}>{tutor['profile']['name']}</Link> </td>
     <td> {tutor['profile']['major']} </td>
     <td> {_(tutor['bio']).truncate(({length: 50}))} </td>
     <td> {_(tutor['exp']).truncate(({length: 50}))} </td>
