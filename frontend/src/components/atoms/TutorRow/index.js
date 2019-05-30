9b4c16ce302849badebf44e7024b5f19e18a52f2
Dongmin Lee <ldm@ldm2468.com>
2019-05-30 17:33:23 +0900

    Temporary workaround for chrome bug

--- frontend/src/components/atoms/TutorRow/index.js
+++ frontend/src/components/atoms/TutorRow/index.js
@@ -4,8 +4,8 @@ import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 
 const TutorRow = ({tutor}) => {
   return typeof tutor === 'undefined' ? null :
-  <tr className="position-relative">
-    <td> <Link className="stretched-link" to={'/profile/' + tutor['profile']['user']}>{tutor['profile']['name']}</Link> </td>
+  <tr>
+    <td> <Link to={'/profile/' + tutor['profile']['user']}>{tutor['profile']['name']}</Link> </td>
     <td> {tutor['profile']['major']} </td>
     <td> {_(tutor['bio']).truncate(({length: 30}))} </td>
     <td> {_(tutor['exp']).truncate(({length: 30}))} </td>
