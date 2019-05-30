548e3fe5f29f0e597de10adbdb303b4bcda839b8
Dongmin Lee <ldm@ldm2468.com>
2019-05-30 17:14:12 +0900

    Implement profile edit feature

--- frontend/src/components/pages/ProfileEditPage/index.js
+++ frontend/src/components/pages/ProfileEditPage/index.js
@@ -4,8 +4,40 @@ import {NavBar} from 'components'
 import { withRouter } from 'react-router-dom';
 
 class ProfileEditPage extends React.Component {
+  constructor(props) {
+    super(props);
+    const profile = this.props.profile;
+    this.state = {name: profile.name, major: profile.major};
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
   componentDidMount() {
+    console.log(this.props);
     this.props.getProfile(this.props.user.id);
+  }
+
+  componentDidUpdate() {
+    if (this.props.profile.id == this.props.user.id) return;
+    this.props.getProfile(this.props.user.id);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event) {
+    const name = this.state['name'] == '' ? this.props.profile.name : this.state['name'];
+    const major = this.state['major'] == '' ? this.props.profile.major : this.state['major'];
+    this.props.putProfile(this.props.profile.id, name, major);
+    event.preventDefault();
   }
 
   render() {
@@ -15,14 +47,14 @@ class ProfileEditPage extends React.Component {
         <NavBar user={user} />
         <div className="container mt-3">
         <h3> 프로파일 정보 수정 </h3>
-        <form className="form mt-3">
+        <form className="form mt-3" onSubmit={this.handleSubmit}>
           <div className="form-group col-md-3">
             <label htmlFor="profileedit-name">이름</label>
-            <input type="text" className="form-control" id="profileedit-name" placeholder={profile.name} />
+            <input type="text" name="name" className="form-control" id="profileedit-name" placeholder={profile.name} onChange={this.handleInputChange} />
           </div>
           <div className="form-group col-md-3">
             <label htmlFor="profileedit-major">전공</label>
-            <input type="text" className="form-control" id="profileedit-major" placeholder={profile.major} />
+            <input type="text" name="major" className="form-control" id="profileedit-major" placeholder={profile.major} onChange={this.handleInputChange} />
           </div>
           <div className="form-group col-md-5" style={{ verticalAlign:'middle' }}>
             <label htmlFor="profileedit-submit">사실 뻥이고 수정 안 돼요</label><br />
--- frontend/src/containers/ProfileEditPage.js
+++ frontend/src/containers/ProfileEditPage.js
@@ -2,14 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { ProfileEditPage } from 'components'
-import { getProfile } from 'store/actions'
+import { getProfile, putProfile } from 'store/actions'
 
 const mapStateToProps = state => {
   return {user: state.user, profile: state.profile, tutor: state.tutor};
 };
 
 const mapDispatchToProps = {
-  getProfile,
+  getProfile, putProfile,
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(ProfileEditPage);
