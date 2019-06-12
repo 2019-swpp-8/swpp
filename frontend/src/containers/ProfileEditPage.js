f80bd356d81fdd27d055afb48eb13358246d857a
Dongmin Lee <ldm@ldm2468.com>
2019-06-12 16:01:04 +0900

    Implement frontend tutor put

--- frontend/src/containers/ProfileEditPage.js
+++ frontend/src/containers/ProfileEditPage.js
@@ -2,14 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { ProfileEditPage } from 'components'
-import { getProfile, putProfile } from 'store/actions'
+import { getProfile, putProfile, putTutor } from 'store/actions'
 
 const mapStateToProps = state => {
   return {user: state.user, profile: state.profile, tutor: state.tutor};
 };
 
 const mapDispatchToProps = {
-  getProfile, putProfile,
+  getProfile, putProfile, putTutor,
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(ProfileEditPage);
