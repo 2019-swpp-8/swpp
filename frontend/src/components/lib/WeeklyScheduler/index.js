e21704c44e36ad859e6a446ee358f1f13e457cf2
Dongmin Lee <ldm@ldm2468.com>
2019-06-16 14:05:10 +0900

    Add weeklyscheduler library to project

---
+++ frontend/src/components/lib/WeeklyScheduler/index.js
@@ -0,0 +1,142 @@
+import React from 'react';
+import debounce from 'lodash/debounce';
+import {DayHeader, TimeRow, EventSelector} from 'components';
+
+
+class WeeklyScheduler extends React.Component {
+  constructor(props) {
+    super(props);
+    const { defaultEvent, selectedEvent, currentSchedule } = this.props;
+    let days = [];
+    if (currentSchedule) {
+      days = currentSchedule;
+    } else {
+      for (let i = 0; i < 7; i += 1) {
+        const day = [];
+        for (let j = 0; j < 48; j += 1) {
+          day.push(defaultEvent);
+        }
+        days.push(day);
+      }
+    }
+    this.state = {
+      days,
+      startingCell: null,
+      currentEvent: selectedEvent || defaultEvent,
+      oldDays: days
+    };
+    this.onMouseDown = this.onMouseDown.bind(this);
+    this.onMouseUp = this.onMouseUp.bind(this);
+    this.onMouseOver = this.onMouseOver.bind(this);
+    this.handleSelectEvent = debounce(this.handleSelectEvent.bind(this), 20);
+  }
+
+  componentWillReceiveProps(newProps) {
+    const { currentSchedule } = this.props;
+    const { days } = this.state;
+    this.setState({
+      days: newProps.currentSchedule || days
+    });
+  }
+  onMouseDown(e) {
+    e.preventDefault();
+    const rowNum = e.target.getAttribute('data-row');
+    const dayNum = e.target.getAttribute('data-day');
+    this.setState({
+      startingCell: {
+        day: parseInt(dayNum, 10),
+        time: parseInt(rowNum, 10)
+      }
+    });
+    this.weekTable.addEventListener('mouseover', this.onMouseOver);
+    window.addEventListener('mouseup', this.onMouseUp);
+  }
+  onMouseUp() {
+    const { days } = this.state;
+    this.weekTable.removeEventListener('mouseover', this.onMouseOver);
+    window.removeEventListener('mouseup', this.onMouseUp);
+    this.setState({ oldDays: days });
+  }
+  onMouseOver(e) {
+    const rowNum = e.target.getAttribute('data-row');
+    const dayNum = e.target.getAttribute('data-day');
+    this.handleDragOver(parseInt(dayNum, 10), parseInt(rowNum, 10));
+  }
+  setupTimeRows() {
+    const { days } = this.state;
+    const rows = [];
+    for (let i = 0; i < 48; i += 1) {
+      const row = [];
+      for (let j = 0; j < 7; j += 1) {
+        row.push(days[j][i]);
+      }
+      rows.push(row);
+    }
+    return rows.map((tRow, index) => (
+      <TimeRow
+        key={index} rowNumber={index} dayItems={tRow}
+      />
+    ));
+  }
+  handleSelectEvent(eventSelected) {
+    const { currentEvent } = this.state;
+    if (eventSelected.event !== currentEvent.event) {
+      this.setState({ currentEvent: eventSelected });
+    }
+  }
+  handleDragOver(dayNum, rowNum) {
+    const { startingCell, currentEvent, oldDays } = this.state;
+
+    const dayDiff = dayNum - startingCell.day;
+    const timeDiff = rowNum - startingCell.time;
+    const newDays = [];
+
+    for (let j = 0; j < 7; j += 1) {
+      newDays.push(oldDays[j].slice());
+    }
+    if (dayDiff !== 0) {
+      const dayStart = (startingCell.day < dayNum) ? startingCell.day : dayNum;
+      const dayEnd = (startingCell.day < dayNum) ? dayNum : startingCell.day;
+      const timeStart = (startingCell.time < rowNum) ? startingCell.time : rowNum;
+      const timeEnd = (startingCell.time < rowNum) ? rowNum : startingCell.time;
+      for (let j = dayStart; j <= dayEnd; j += 1) {
+        if (timeDiff !== 0) {
+          for (let i = timeStart; i <= timeEnd; i += 1) {
+            newDays[j][i] = currentEvent;
+          }
+        } else {
+          newDays[j][startingCell.time] = currentEvent;
+        }
+      }
+    } else {
+      const timeStart = (startingCell.time < rowNum) ? startingCell.time : rowNum;
+      const timeEnd = (startingCell.time < rowNum) ? rowNum : startingCell.time;
+      for (let j = timeStart; j <= timeEnd; j += 1) {
+        newDays[startingCell.day][j] = currentEvent;
+      }
+    }
+    this.setState({ days: newDays });
+  }
+  render() {
+    const { events, defaultEvent } = this.props;
+    const { currentEvent } = this.state;
+    return (
+      <div id="WeeklySchedulerTable">
+        <EventSelector
+          events={events} selectedEvent={currentEvent} selectEvent={this.handleSelectEvent}
+        />
+        <table>
+          <DayHeader />
+          <tbody
+            className="week-table" onMouseDown={this.onMouseDown}
+            ref={(tbody) => { this.weekTable = tbody; }}
+          >
+            {this.setupTimeRows()}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default WeeklyScheduler;
