12a28d2f3d51ac66067ae98dd2e641e9813c282a
Dongmin Lee <ldm@ldm2468.com>
2019-06-17 04:49:42 +0900

    Fix cases with two buttons

--- frontend/src/components/atoms/RequestRow/index.js
+++ frontend/src/components/atoms/RequestRow/index.js
@@ -13,11 +13,11 @@ const RequestRow = ({user, request, deleteRequest, changeStatus}) => {
   const detail = request['detail']
   const payment = request['payment']
 
-  const accept_button = <button id="request-accept" onClick={()=>changeStatus(1)} className="btn btn-outline-primary ml-2">수락하기</button>;
-  const complete_button = <button id="request-complete" onClick={()=>changeStatus(2)} className="btn btn-outline-primary ml-2">완료하기</button>;
-  const star_button = <button id="request-star" onClick={()=>deleteRequest()} className="btn btn-outline-primary ml-2">평점주기</button>;
-  const cancel_button = <button id="request-cancel" onClick={()=>deleteRequest()} className="btn btn-outline-danger ml-2">취소하기</button>;
-  const times_button = <button id="request-times" data-toggle="modal" data-target={'#modal-' + request['id']} className="btn btn-outline-dark btn">시간대 확인하기</button>;
+  const accept_button = <button id="request-accept" onClick={()=>changeStatus(1)} className="btn btn-outline-primary">수락</button>;
+  const complete_button = <button id="request-complete" onClick={()=>changeStatus(2)} className="btn btn-outline-primary">완료</button>;
+  const star_button = <button id="request-star" onClick={()=>deleteRequest()} className="btn btn-outline-primary">평점</button>;
+  const cancel_button = <button id="request-cancel" onClick={()=>deleteRequest()} className="btn btn-outline-danger">취소</button>;
+  const times_button = <button id="request-times" data-toggle="modal" data-target={'#modal-' + request['id']} className="btn btn-outline-dark mt-1">시간대 확인</button>;
 
   const active_button = status == 0 && user == request['tutor']['profile']['user'] ? accept_button :
       status == 1 ? complete_button :
@@ -43,6 +43,9 @@ const RequestRow = ({user, request, deleteRequest, changeStatus}) => {
     </div>
   </div>;
 
+  const two_buttons = <div className="btn-group mt-1" role="group"> {active_button} {cancel_button} </div>;
+  const single_button = <div className="mt-1"> {active_button} </div>;
+
   return <div className="card mb-2">
     <div className="card-body">
       <div className="row">
@@ -59,10 +62,8 @@ const RequestRow = ({user, request, deleteRequest, changeStatus}) => {
           {modal} </div>
         </div>
         <div className="col-md-2">
-        상태:<br />
-        {_(status == 0 ? "대기중" : status == 1 ? "진행중" : "완료됨").truncate(({length: 30}))}
-          {active_button}
-          {status == 0 ? cancel_button : null}
+        상태: {_(status == 0 ? "대기중" : status == 1 ? "진행중" : "완료됨").truncate(({length: 30}))}
+          { status == 0 ? two_buttons : single_button }
         </div>
       </div>
     </div>
