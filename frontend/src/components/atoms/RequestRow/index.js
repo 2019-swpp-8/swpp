3d7127efe3d0f0487299a7149fb7908a5897d4db
Dongmin Lee <ldm@ldm2468.com>
2019-06-16 22:48:50 +0900

    Show request times as modal

--- frontend/src/components/atoms/RequestRow/index.js
+++ frontend/src/components/atoms/RequestRow/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import _ from 'lodash'
+import {WeeklyScheduler} from 'components'
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 
 const RequestRow = ({user, request, deleteRequest, changeStatus}) => {
@@ -16,10 +17,32 @@ const RequestRow = ({user, request, deleteRequest, changeStatus}) => {
   const complete_button = <button id="request-complete" onClick={()=>changeStatus(2)} className="btn btn-primary mb-2">완료</button>;
   const star_button = <button id="request-star" onClick={()=>deleteRequest()} className="btn btn-primary mb-2">평점</button>;
   const cancel_button = <button id="request-cancel" onClick={()=>deleteRequest()} className="btn btn-danger mb-2">취소</button>;
+  const times_button = <button id="request-times" data-toggle="modal" data-target={'#modal-' + request['id']} className="btn btn-primary mb-2">시간</button>;
 
   const active_button = status == 0 && user == request['tutor']['profile']['user'] ? accept_button :
       status == 1 ? complete_button :
       status == 2 && user == request['tutee']['user'] ? star_button : null;
+
+  const modal =
+  <div className="modal fade" id={'modal-' + request['id']} tabIndex="-1" role="dialog" aria-hidden="true">
+    <div className="modal-dialog" role="document">
+      <div className="modal-content">
+        <div className="modal-header">
+          <h5 className="modal-title" id="exampleModalLabel">튜터링 시간</h5>
+          <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div className="modal-body">
+          <WeeklyScheduler times={request['times']} readonly={true} />
+        </div>
+        <div className="modal-footer">
+          <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="button" className="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>;
   return <tr>
     <td> {tutor_name} </td>
     <td> {_(tutee_name).truncate(({length: 30}))} </td>
@@ -29,8 +52,11 @@ const RequestRow = ({user, request, deleteRequest, changeStatus}) => {
     <td> {_(status == 0 ? "대기중" : status == 1 ? "진행중" : "완료").truncate(({length: 30}))}
       {active_button}
       {status == 0 ? cancel_button : null}
+      {times_button}
+      {modal}
     </td>
   </tr>
+
 };
 
 export default RequestRow;
