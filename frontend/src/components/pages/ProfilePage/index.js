d3c9c3e70ad148bcbe3e3962aff42cb50abbb974
Dongmin Lee <ldm@ldm2468.com>
2019-06-16 23:56:19 +0900

    Make Profile page double column

--- frontend/src/components/pages/ProfilePage/index.js
+++ frontend/src/components/pages/ProfilePage/index.js
@@ -18,7 +18,7 @@ class ProfilePage extends React.Component {
   render() {
     const {user, profile, tutor, requestlist, deleteRequest, changeRequestStatus} = this.props;
     const editButton = profile.id == user.id ?
-      <Link to='/profile/edit' className="btn btn-outline-primary login-button">편집</Link> :
+      <Link to='/profile/edit' className="btn btn-outline-primary login-button">수정</Link> :
       "";
     const lectureList = Array.isArray(tutor.lectures) ? tutor.lectures.map(i => (
       <tr key={i.id}><td>{i.prof}</td><td>{i.title}</td></tr>
@@ -33,33 +33,37 @@ class ProfilePage extends React.Component {
       <div>
         <NavBar user={user} />
         <div className="container mt-3">
-          <h3> 프로파일 정보 </h3>
-          <div> 이름: {profile.name} </div>
-          <div> 전공: {profile.major} </div>
-          <div> 연락처: {profile.contact} </div>
-          <br/>
-          <h3> 튜터 정보 </h3>
-          <div> 소개: {tutor.bio} </div>
-          <div className="mb-1"> 경력: {tutor.exp} </div>
-          <br/>
-          <h3> 수강한 강의 </h3>
-          <table className="table table-hover">
-            <thead>
-              <tr>
-                <th> 교수님 </th>
-                <th> 강의명 </th>
-              </tr>
-            </thead>
-            <tbody>
-              {lectureList}
-            </tbody>
-          </table>
           <div className="row">
-            <div className="col-5">
+            <div className="col-md-5">
+              <h3> 프로파일 정보 </h3>
+              <div> 이름: {profile.name} </div>
+              <div> 전공: {profile.major} </div>
+              <div> 연락처: {profile.contact} </div>
+              <br/>
+              <h3> 튜터 정보 </h3>
+              <div> 소개: {tutor.bio} </div>
+              <div className="mb-1"> 경력: {tutor.exp} </div>
+              <br/>
+              <h3> 수강한 강의 </h3>
+              <table className="table table-hover">
+                <thead>
+                  <tr>
+                    <th> 교수님 </th>
+                    <th> 강의명 </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {lectureList}
+                </tbody>
+              </table>
+
+              {editButton}
+            </div>
+            <div className="col-md-5">
+              <h3> 튜터링 가능 시간대 </h3>
               <WeeklyScheduler times={tutor.times} tutoringTimes={tutor.tutoringTimes} readonly={true} inv={false}/>
             </div>
           </div>
-          {editButton}
           {profile.id == user.id ? (
             <div className="mt-3">
               <h3> 튜터링 해주는 목록 </h3>
